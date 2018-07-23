30c210d8ecf8c2afe841baaf2a77943fa020ed79
rajesh <squashmeister99@gmail.com>
2018-07-23 10:29:32 -0700

    added more code logic

--- Hangman/Hangman/Hangman.py
+++ Hangman/Hangman/Hangman.py
@@ -48,14 +48,36 @@ def playAgain():
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_PICS[len(missedLetters)])
+    print()
+
+    print('Missed letters:', end= ' ')
+    for letter in missedLetters:
+        print(letter, end = ' ')
+    print()
+
+    blanks = '_'*len(secretWord)
+    for i in range(len(secretWord)):
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    # print current status
+    for letter in blanks:
+        print(letter, end = ' ')
+    print()
+
 def main():
-    for item in HANGMAN_PICS:
-        print(item)
+    print('HANGMAN')
+    missedLetters = ''
+    correctLetters = ''
+    secretWord = getRandomWord()
+    gameIsDone = False
 
-    for item in WORD_LIST:
-        print(getRandomWord())
 
-    print(playAgain())
+    while not gameIsDone:
+        displayBoard(missedLetters, correctLetters, secretWord)
+        gameIsDone = not playAgain()
 
 if __name__ == "__main__":
     main()
