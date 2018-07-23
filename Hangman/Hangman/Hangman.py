c34d2187cc790906fb5e09620705e48c1e359d96
rajesh <squashmeister99@gmail.com>
2018-07-23 10:10:23 -0700

    added more words and helper methods

--- Hangman/Hangman/Hangman.py
+++ Hangman/Hangman/Hangman.py
@@ -37,11 +37,25 @@ HANGMAN_PICS = ['''
    / \  |
        ===''']
 
+WORD_LIST = '''cat dog apple banana spider stork eagle ferret panda shark anaconda crocodile alligator pirhana
+                orca '''.split()
+
+#fetch a random word from the word list
+def getRandomWord():
+    return WORD_LIST[random.randint(0, len(WORD_LIST) - 1)]
+
+def playAgain():
+    print('Do you want to play again? (yes or no)')
+    return input().lower().startswith('y')
 
 def main():
     for item in HANGMAN_PICS:
         print(item)
-        print()
+
+    for item in WORD_LIST:
+        print(getRandomWord())
+
+    print(playAgain())
 
 if __name__ == "__main__":
     main()
