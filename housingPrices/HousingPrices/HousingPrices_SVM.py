75bbd7cc42f325bda3274eda18c4f7b76c7b33c9
Rajesh Vaidya <squashmeister99@gmail.com>
2019-09-12 21:53:17 -0700

    changes

--- housingPrices/HousingPrices/HousingPrices_SVM.py
+++ housingPrices/HousingPrices/HousingPrices_SVM.py
@@ -1,80 +1,119 @@
-
-# Code you have previously used to load data
-import pandas as pd
+from sklearn.ensemble import (RandomForestRegressor, IsolationForest)
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import Imputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import (train_test_split, GridSearchCV)
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import (StandardScaler, OneHotEncoder, FunctionTransformer, KBinsDiscretizer)
 from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
+from sklearn.compose import ColumnTransformer
+from scipy import stats
+import numpy as np
+import pandas as pd
 
-# Path of the file to read. We changed the directory structure to simplify submitting to a competition
+# Data Visualisation
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# plot correlations
+def plotCoorelations(df):
+    # remove non_numeric features  
+    corr = df.corr()
+    corr.style.background_gradient(cmap='coolwarm').set_precision(2)
+
+# define a method to use Isolation Forest for outlier detection
+def outlierDetect_IsolationForest(home_data):
+    clf = IsolationForest( behaviour = 'new', contamination= 0.02)
+    preds = clf.fit_predict(home_data)
+    outliers = np.where(preds == -1)
+
+    # drop outliers from home data
+    home_data_out = np.delete(home_data.todense(), outliers[0], axis = 0)
+    print("number of outliers = {0}".format(len(outliers[0])))
+
+    return home_data_out, outliers
+
+
+# define a method to use Isolation Forest for outlier detection
+def outlierDetect_ZScore(home_data, zValue = 3):
+    X_numeric = home_data.select_dtypes(exclude=object) 
+    
+    z = np.abs(stats.zscore(X_numeric))
+    outliers = np.where(z > zValue)
+
+    # drop outliers from home data
+    home_data_out = home_data.drop(labels = outliers[0], inplace=False, errors = "ignore")
+    #home_data_out = home_data[(preds == -1).all(axis=1)]
+    print("number of outliers = {0}".format(len(outliers[0])))
+    return home_data_out, outliers
+
+
+# load housing data
 iowa_file_path = '../data/train.csv'
-
 home_data = pd.read_csv(iowa_file_path)
+Y = home_data["SalePrice"]
+X = home_data.drop(columns = ["Id", "SalePrice"])
 
-# Create target object and call it y
-y = home_data.SalePrice
+# split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=1)
+print(X_train.shape)
+
+# perform feature scaling
+
+numeric_features = X_train.select_dtypes(exclude=object) 
+num_features_names = numeric_features.columns
+
+# features that need a log transformation
+log_features_names = ["LotFrontage", "LotArea", "1stFlrSF", "GrLivArea", "OpenPorchSF"]
+
+log_pipeline = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', FunctionTransformer(np.log1p, validate=False)),
+    ('logscaler', StandardScaler())])
+
+#kbinDiscretizer features
+year_features_names = ['YearBuilt', 'YearRemodAdd', 'GarageYrBlt', 'YrSold']
+year_pipeline = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('kbd', KBinsDiscretizer(n_bins=5, encode='onehot-dense'))])
+
+#numeric features that require a normal transformation
+numeric_features_names = [x for x in num_features_names if x not in log_features_names + year_features_names]
+print(len(numeric_features_names))
+
+numeric_pipeline = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())])
+
+#categorical features
+categorical_features_names = X_train.select_dtypes(include=object).columns
+
+cat_pipeline = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+transformers=[
+        ('log', log_pipeline, log_features_names),
+        ('num', numeric_pipeline, numeric_features_names),
+        ('cat', cat_pipeline, categorical_features_names),
+        ('year', year_pipeline, year_features_names)     
+    ]
+
+ct = ColumnTransformer(transformers=transformers)
+Xt = ct.fit_transform(X_train)
+
+# perform mean normalization and feature scaling on the data
+print(y_train.iloc[766])
+X_train_t, outliers = outlierDetect_IsolationForest(Xt)
+y_train = np.delete(y_train.values, outliers[0])
+
+clf = RandomForestRegressor(random_state=1, n_estimators = 300, criterion="mae")
+
+# fit the log of the sale price
+clf.fit(X_train_t, np.log1p(y_train))
+
+y_predict = clf.predict(ct.transform(X_test))
+
+rf_val_mae = mean_absolute_error(y_test, np.expm1(y_predict))
+
+print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
 
 
-# drop the Id column and sales price column
-home_data = home_data.drop("Id", axis=1)
-home_data = home_data.drop("SalePrice", axis=1)
-print(home_data.info())
-
-# get info on colums that have numeric types
-numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
-
-X = home_data.select_dtypes(include=numerics)
-
-# Split into validation and training data
-train_X, val_X, train_y, val_y = train_test_split(X, y, train_size = 0.85, random_state=1)
-# do feature scaling and normalization on training data
-num_pipeline = Pipeline([
-    ('imputer', Imputer(strategy="median")),
-    ('std_scaler', StandardScaler())])
-
-# normalize the training data
-train_X_scaled = num_pipeline.fit_transform(train_X)
-
-# normalize test data using teh parameters learned from training data
-val_X_scaled = num_pipeline.transform(val_X)
-
-# Define the model. Set random_state to 1
-svm_model = LinearSVC(loss="hinge", max_iter=50000)
-
-svm_model.fit(train_X_scaled, train_y)
-svm_val_predictions = svm_model.predict(val_X_scaled)
-svm_val_mae = mean_absolute_error(svm_val_predictions, val_y)
-
-print("Validation MAE for LinearSVC Model: {:,.0f}".format(svm_val_mae))
-
-# now try a grid search to get optimal hyperparameters
-param_grid = [{'n_estimators': [ 300, 500]},
-              ]
-# train the best model on the full data
-full_x_scaled = num_pipeline.fit_transform(X)
-svm_model_full = LinearSVC(loss="hinge", C=10,  max_iter=20000)
-svm_model_full.fit(full_x_scaled, y)
-
-
-# path to file you will use for predictions
-test_data_path = '../data/test.csv'
-
-test_data = pd.read_csv(test_data_path)
-# drop the Id column
-test_data2 = test_data.drop("Id", axis=1)
-
-# get numeric columns & normalize data
-test_X = test_data2.select_dtypes(include=numerics)
-test_X_scaled = num_pipeline.transform(test_X)
-
-# make predictions which we will submit. 
-test_preds = svm_model_full.predict(test_X_scaled)
-
-# The lines below shows how to save predictions in format used for competition scoring
-
-output = pd.DataFrame({'Id': test_data.Id,
-                       'SalePrice': test_preds})
-output.to_csv('../data/submission.csv', index=False)
