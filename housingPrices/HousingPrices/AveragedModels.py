ab1819a01c9a1b67a173ea501b9952ccc6e6e819
Rajesh Vaidya <squashmeister99@gmail.com>
2019-09-27 14:14:59 -0700

    averaged model

--- housingPrices/HousingPrices/AveragedModels.py
+++ housingPrices/HousingPrices/AveragedModels.py
@@ -1,13 +1,15 @@
 from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
 from sklearn.model_selection import KFold, cross_val_score, train_test_split
+import numpy as np
 
 class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
     def __init__(self, models):
-        self.models = models
+        self.models_ = models
+        print(self.models_)
         
     # define clones of the original models to fit the data in
     def fit(self, X, y):
-        self.models_ = [clone(x) for x in self.models]
+        self.models_ = [clone(x) for x in self.models_]
         
         # Train cloned base models
         for model in self.models_:
@@ -17,6 +19,7 @@ class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
     
     #do the predictions for cloned models and average them
     def predict(self, X):
+        print(self.models_)
         predictions = np.column_stack([
             model.predict(X) for model in self.models_
         ])
