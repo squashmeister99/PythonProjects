7d95b3c63d422527d101bcd20d3bf57bf479a5b0
Rajesh Vaidya <squashmeister99@gmail.com>
2020-04-28 10:47:36 -0700

    cleanup of the file

--- drawGrid/drawGrid.py
+++ drawGrid/drawGrid.py
@@ -7,6 +7,20 @@ from time import perf_counter, sleep
 PEN_COLOR = "blue"
 BG_COLOR = "white"
 FILL_COLOR = "black"
+BOX_SIZE = 16
+RADIUS = BOX_SIZE/4
+
+# draws a circle of the specified radius
+def draw_circle(t, x, y, fill_color = BG_COLOR):
+    t.penup() # no drawing!
+    t.goto(x,y) # move the pen to a different position
+    t.pendown() # resume drawing
+    t.pen(pencolor="purple" , fillcolor = "orange", pensize = 10, speed = 0 )
+    t.begin_fill()
+    t.circle(RADIUS)
+    t.end_fill()
+    return
+
 
 ' draws a box using turtle graphics'
 def draw_box(t,x,y,size,fill_color = BG_COLOR):
@@ -14,44 +28,53 @@ def draw_box(t,x,y,size,fill_color = BG_COLOR):
     t.goto(x,y) # move the pen to a different position
     t.pendown() # resume drawing
     
-    t.fillcolor(fill_color)
-    t.begin_fill()  # Shape drawn after this will be filled with this color!
- 
     for i in range(0,4):
         t.forward(size) # move forward
         t.right(90) # turn pen right 90 degrees
  
-    t.end_fill() 
+    return
  
- 
+def playGame(iterations, x_dim, y_dim):
+    for x in range(iterations):   
+           
+        t1_start = perf_counter()       
+        num_squares = x_dim*y_dim    
+        t1_stop = perf_counter() 
+        print("Elapsed time for loop: {0} sec", t1_stop - t1_start)
+
+def setupScreen():
+    screen = turtle.Screen()
+    screen.bgcolor(BG_COLOR)
+    screen.title('Game of Life')
+    screen.tracer(0, 0)
+    return screen
+
+def setupGridTurtle():
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.pen(pencolor = PEN_COLOR, speed = 0)
+    return t
+
 def draw_board(args):
+
+    screen = setupScreen()
+    t = setupGridTurtle()
+    
+    start_x = 0 # starting x position of the board
+    start_y = 0 # starting y position of the board
+
     x_dim = args.x
     y_dim = args.y
     iterations = args.i
 
-    turtle.bgcolor(BG_COLOR)
-    screen = turtle.Screen()
-    board = turtle.Turtle()
-    board.pencolor(PEN_COLOR)
-    board.speed(0)
-    screen.tracer(0, 0)
-      
-    for x in range(iterations):   
-        
-        
-        t1_start = perf_counter()
-        start_x = 0 # starting x position of the board
-        start_y = 0 # starting y position of the board
-        box_size = 15 # pixel size of each square in the board
-        num_squares = x_dim*y_dim
-        for i in range(0, x_dim ): 
-            for j in range(0, y_dim ):
-                square_color = FILL_COLOR if random.randrange(1, num_squares) > int(0.75*(num_squares)) else BG_COLOR         
-                draw_box(board, start_x+j*box_size,start_y+i*box_size,box_size,square_color)
+    for i in range(0, x_dim ): 
+            for j in range(0, y_dim ):    
+                draw_box(t, start_x+j*BOX_SIZE, start_y+i*BOX_SIZE, BOX_SIZE)
 
-        screen.update()   # update the screen 
-        t1_stop = perf_counter() 
-        print("Elapsed time for loop: {0} sec", t1_stop - t1_start) 
+    
+    screen.update()   # update the screen 
+      
+    #playGame(iterations, x_dim, y_dim) 
 
     screen.exitonclick()
 
